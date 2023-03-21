d5ad5bd5044b6f5c11f30044ff744099ca943067
Andrii Shubin <shubinanri@gmail.com>
2023-03-21 11:59:15 +0200

    Update MovieDetails.jsx

--- src/Pages/MovieDetails/MovieDetails.jsx
+++ src/Pages/MovieDetails/MovieDetails.jsx
@@ -3,8 +3,6 @@ import { NavLink, useNavigate, useLocation, useParams } from "react-router-dom";
 import emptyPhoto from 'components/images/no_photo.png';
 import css from 'Pages/MovieDetails/MovieDetails.module.css';
 import { getMovieDetails } from 'components/Api/api.jsx';
-// import { Cast } from 'Pages/MovieDetails/Cast/Cast.jsx';
-// import { Reviews } from 'Pages/MovieDetails/Reviews/Reviews.jsx';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -17,9 +15,6 @@ export const MovieDetails = () => {
 
   useEffect(() => { getMovieDetails(id).then(setCurrentMovies) }, [id]);
 
-
-  // const [showCast, setShowCast] = useState(false)
-  // const [showReviews, setReviews] = useState(false)
   const [toggleCast, setToggleCast] = useState('cast')
   const onToggleCast = () => {
     setToggleReviews("reviews");
@@ -31,20 +26,6 @@ export const MovieDetails = () => {
     setToggleCast("cast");
     setToggleReviews(prevState => prevState === "reviews" ? "" : "reviews");
   };
-
-  // const onCast = () => {
-  //   setShowCast(prevState => !prevState);
-  //   setReviews(false);
-  //   onToggleCast();
-  //   setToggleReviews('reviews')
-  // };
-
-  // const onReviews = () => {
-  //   setReviews(prevState => !prevState);
-  //   setShowCast(false);
-  //   onToggleReviews();
-  //   setToggleCast('cast')
-  // };
 
   const onBack = () => {
     navigate(location?.state?.from || '/');
@@ -73,13 +54,6 @@ export const MovieDetails = () => {
         onClick={onToggleCast}
         className={({ isActive }) => (isActive ? css.active : css.inactive)}
          to={toggleCast} state={{ from: location.state?.from ?? '/' }}>
-          {/* <button className={css.but} 
-            type="button"
-            name="ShowCast"
-            onClick={onCast}
-          >
-            Cast
-          </button> */}
           Cast
         </NavLink>
 </li>
@@ -87,17 +61,8 @@ export const MovieDetails = () => {
 <li>
 <NavLink 
         onClick={onToggleReviews}
-        // className="Nav_link"
-        // activeClassName="activeRoute"
         className={({ isActive }) => (isActive ? css.active : css.inactive)} 
         to={toggleReviews} state={{ from: location.state?.from ?? '/' }}>
-          {/* <button className={css.but} 
-            type="button"
-            name="ShowReviews"
-            onClick={onReviews}
-          >
-            Reviews
-          </button> */}
           Reviews
         </NavLink>
 </li>
@@ -116,11 +81,6 @@ export const MovieDetails = () => {
       <Suspense fallback={null}>
                         <Outlet />
                     </Suspense>
-
-      {/* {showCast && <Cast
-      />}
-      {showReviews && <Reviews
-      />} */}
     </div>
   );
 };
